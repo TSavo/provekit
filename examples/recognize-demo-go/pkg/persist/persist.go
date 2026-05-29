f68d6dd586f42d9666559d214e41a7c14c23622e
T Savo <kevlar.sindome@gmail.com>
2026-05-29 08:38:13 -0700

    Add Go recognize demo (#1590)

---
+++ examples/recognize-demo-go/pkg/persist/persist.go
@@ -0,0 +1,18 @@
+package persist
+
+import (
+	"database/sql"
+
+	_ "github.com/provekit/recognize-demo-go/internal/memsqlite"
+)
+
+func OpenStore(driver string, dsn string) *sql.DB {
+	return mustDB(sql.Open(driver, dsn))
+}
+
+func mustDB(db *sql.DB, err error) *sql.DB {
+	if err != nil {
+		panic(err)
+	}
+	return db
+}
